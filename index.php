245211bffc6a6e3d4136b744554f261b00796fbc
wyu <kharma@gmail.com>
2016-02-16 08:48:12 -0800

    p2: Update page title

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
   <div class="container">
     <div class="text-left">
       <h1>XKCD Password Generator</h1>
+      <h3>DWA-15, Spring 2016</h3>
 
       <form action='index.php' method='GET'>
         <!--
